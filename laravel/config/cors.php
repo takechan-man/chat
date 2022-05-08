194178dfe07c99734151c0ff7777f6bb0c159e8d
mtk920 <matsu9.tigers20@gmail.com>
2022-05-08 20:07:53 +0900

    fix: cors設定

--- laravel/config/cors.php
+++ laravel/config/cors.php
@@ -17,18 +17,18 @@ return [
 
     'paths' => ['api/*'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET, POST, PUT, PATCH, DELETE, OPTIONS'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://localhost:3000'],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['Content-Type, Origin, Authorization'],
 
     'exposed_headers' => [],
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
